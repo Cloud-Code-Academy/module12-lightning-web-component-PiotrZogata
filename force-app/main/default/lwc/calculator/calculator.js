0dd7e95a5e9125f05cab60d7bc0d702869af2a80
Piotr Zogata <piotrzogata@gmail.com>
2025-02-24 12:11:20 +0100

    handleChange(event)

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,3 +1,28 @@
 import { LightningElement } from 'lwc';
 
-export default class Calculator extends LightningElement {}
+export default class Calculator extends LightningElement {
+
+    devFundamentalScore = 0;
+    processAutomationScore = 0;
+    userInterfaceScore = 0;
+    testingDebbugingDeploymentScore = 0;
+
+    certificationScore = 0;
+    
+    calculateScore(){
+        this.certificationScore = (this.devFundamentalScore + this.processAutomationScore + this.userInterfaceScore + this.testingDebbugingDeploymentScore);
+    }
+
+    handleChange(event){
+        let value = Number(event.target.value);
+        if(event.target.name === 'devFundamentals'){
+            this.devFundamentalScore = value;
+        }else if(event.target.name === 'process_Automation'){
+            this.processAutomationScore = value;
+        }else if(event.target.name === 'userInterface'){
+            this.userInterfaceScore = value;
+        }else if(event.target.name === 'testingDebbugingDeployment'){
+            this.testingDebbugingDeploymentScore = value;
+        }
+    }
+}
